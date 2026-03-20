04dfaa8ab8c499e38a6e4b5542e3fa9447e32708
kadeksrd <kadeksuryard13@gmail.com>
2026-03-20 12:25:42 +0700

    fixed benefits no declared

--- src/app/(main)/course/[slug]/LandingPageContent.tsx
+++ src/app/(main)/course/[slug]/LandingPageContent.tsx
@@ -16,10 +16,14 @@ export function LandingPageContent({
   landing_page,
   product,
   course,
+  benefits,
+  testimonials,
 }: {
   landing_page: any;
   product: any;
   course: any;
+  benefits?: any;
+  testimonials?: any;
 }) {
   // HAPUS BARIS: const { landing_page, product, course } = data; (Karena sudah diterima di atas)
 
